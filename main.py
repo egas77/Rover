c7373a2b6fcb36ddcf7dbcd236e927184cfbde2b
Egor Koder <ebedak2003@yandex.ru>
2020-01-04 14:05:44 +0700

    Complete feature 'select_level_screen'

--- main.py
+++ main.py
@@ -14,12 +14,6 @@ tiles_texture_folder = 'data\\textures\\png\\tiles'
 background_path = os.path.join(element_texture_folder, 'background.png')
 
 
-def terminate():
-    """Function terminate this game"""
-    pygame.quit()
-    sys.exit(0)
-
-
 def load_image(path, color_key=None):
     image = pygame.image.load(path)
     if color_key is not None:
@@ -32,24 +26,38 @@ def load_image(path, color_key=None):
     return image
 
 
-class StartGameScreen(pygame.Surface):
-    def __init__(self):
-        super().__init__(SIZE)
-        self.fill(pygame.Color('blue'))
-        self.set_colorkey(self.get_at((0, 0)))
+class SelectLevelSprite(pygame.sprite.Sprite):
+    font = pygame.font.Font(None, 100)
+    size_sprite_level = (100, 100)
+    background_image_level = load_image(os.path.join(element_texture_folder, 'box1.png'))
+    background_image_level = pygame.transform.scale(background_image_level, size_sprite_level)
 
-    def show(self):
-        screen.blit(self, (0, 0))
+    def __init__(self, center_x, center_y, number_level):
+        super().__init__(levels_group)
+        self.image = self.background_image_level.copy()
+        self.rect = self.image.get_rect(centerx=center_x, centery=center_y,
+                                        size=self.size_sprite_level)
+        self.number_level = number_level
+        self.number_level_sprite = self.font.render(str(number_level), 1,
+                                                    pygame.color.Color(206, 225, 66))
+        self.image.blit(self.number_level_sprite,
+                        (self.image.get_width() // 2 - self.number_level_sprite.get_width() // 2,
+                         self.image.get_height() // 2 - self.number_level_sprite.get_height() // 2))
+
+
+def terminate():
+    """Function terminate this game"""
+    pygame.quit()
+    sys.exit(0)
 
 
 def start_game():
     start_game_screen = pygame.Surface(SIZE)
-    start_game_screen = start_game_screen.convert()
     start_game_screen.set_colorkey(start_game_screen.get_at((0, 0)))
     button_radius = 50
     button_rect = pygame.Rect(WIDTH // 2 - button_radius, HEIGHT // 2 - button_radius + 100,
                               button_radius * 2, button_radius * 2)
-    pygame.draw.circle(start_game_screen, pygame.Color(14, 14, 232),
+    pygame.draw.circle(start_game_screen, pygame.Color(250, 250, 232),
                        button_rect.center, button_radius)
     name_game = load_image(os.path.join(element_texture_folder, 'name-game.png'))
     start_game_screen.blit(name_game, (0, 0))
@@ -63,11 +71,42 @@ def start_game():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if (event.button == pygame.BUTTON_LEFT and
                         button_rect.collidepoint(event.pos[0], event.pos[1])):
-                    print('Click Start Game')
+                    number_level = select_level()
+                    load_level(number_level)
+                    return None
+
+
+def select_level():
+    screen.blit(background_image, (0, 0))
+    center_x, center_y = WIDTH // 2, HEIGHT // 2
+    size_sprite_level = SelectLevelSprite.size_sprite_level
+    indent_level_px = 50
+    SelectLevelSprite(center_x - size_sprite_level[0] * 2 - indent_level_px * 2, center_y, 1)
+    SelectLevelSprite(center_x - size_sprite_level[0] - indent_level_px, center_y, 2)
+    SelectLevelSprite(center_x, center_y, 3)
+    SelectLevelSprite(center_x + size_sprite_level[0] + indent_level_px, center_y, 4)
+    SelectLevelSprite(center_x + size_sprite_level[0] * 2 + indent_level_px * 2, center_y, 5)
+    levels_group.draw(screen)
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_LEFT:
+                pos = event.pos
+                for level_sprite in levels_group.sprites():
+                    if level_sprite.rect.collidepoint(pos):
+                        return level_sprite.number_level
+
+
+def load_level(number_level):
+    print(number_level)
 
 
 background_image = load_image(background_path)
 background_image = pygame.transform.scale(background_image, SIZE)
+all_sprite = pygame.sprite.Group()
+levels_group = pygame.sprite.Group()
 start_game()
 
 while True:
